49670974b68f3e11b6720b4384d4a04367ddb83b
paadilaenutus <toomistu@gmail.com>
2020-09-15 10:09:39 +0300

    WO clean

--- src/main/java/ee/valiit/project/Repository/RepositoryWO.java
+++ src/main/java/ee/valiit/project/Repository/RepositoryWO.java
@@ -1,5 +1,6 @@
 package ee.valiit.project.Repository;
 
+import ee.valiit.project.Request.RequestWorkOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,13 +13,13 @@ public class RepositoryWO {
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
-    public void createWO(int deviceId, String jobDescription, String technicianName) {
+    public void createWO(RequestWorkOrder createWO) {
         String sql = "INSERT INTO work_orders (device_id, job_description, technician_name) " +
                 "VALUES (deviceId, jobDescription, technicianName)";
         Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("deviceId", deviceId);
-        paramMap.put("jobDescription", jobDescription);
-        paramMap.put("technicianName", technicianName);
+        paramMap.put("deviceId", createWO.getDeviceId());
+        paramMap.put("jobDescription", createWO.getJobDescription());
+        paramMap.put("technicianName", createWO.getTechnicianName());
         jdbcTemplate.update(sql, paramMap);
     }
 }
--- src/main/java/ee/valiit/project/Service/ServiceWO.java
+++ src/main/java/ee/valiit/project/Service/ServiceWO.java
@@ -1,6 +1,7 @@
 package ee.valiit.project.Service;
 
 import ee.valiit.project.Repository.RepositoryWO;
+import ee.valiit.project.Request.RequestWorkOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ public class ServiceWO {
     @Autowired
     public RepositoryWO repositoryWO;
 
-    public void createWO(int deviceId, String jobDescription, String technicianName) {
-        repositoryWO.createWO(deviceId, jobDescription, technicianName);
+    public void createWO(RequestWorkOrder createWO) {
+        repositoryWO.createWO(createWO);
     }
 }
